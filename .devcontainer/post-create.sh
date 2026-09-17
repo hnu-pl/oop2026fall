5b273d3ab89dda87f97b7e09fb592c2e609a0c60
Ki Yung Ahn <kyagrd@gmail.com>
2026-09-17 02:42:17 +0000

    devcontainer config 2

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-if ! command -v jbang >/dev/null 2>&1; then
-  curl -Ls https://sh.jbang.dev | bash -s - app setup
-fi
-
+curl -Ls https://sh.jbang.dev | bash -s - app setup
 export PATH="$HOME/.jbang/bin:$PATH"
 jbang --java 25 io.github.padreati:rapaio-jupyter-kernel:4.0.0 -i -auto
