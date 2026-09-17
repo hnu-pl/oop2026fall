88e11436b8a128bfff0fccd5df218f8830dbcad4
Ki Yung Ahn <kyagrd@gmail.com>
2026-09-17 02:49:52 +0000

    remove devcontainer

--- .devcontainer/post-create.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-curl -Ls https://sh.jbang.dev | bash -s - app setup
-export PATH="$HOME/.jbang/bin:$PATH"
-jbang --java 25 io.github.padreati:rapaio-jupyter-kernel:4.0.0 -i -auto
